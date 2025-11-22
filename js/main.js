cf29467f4f38737aa68b468742a75dc65dab4485
wald52 <wald52@protonmail.com>
2025-11-22 17:56:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
 import { ENTRIES } from './entries.js';
-
-console.log(ENTRIES);
-
-const container = document.getElementById('entries-container');
-if (container) {
-  ENTRIES.forEach(entry => {
-    const li = document.createElement('li');
-    li.textContent = entry;
-    container.appendChild(li);
-  });
-}
